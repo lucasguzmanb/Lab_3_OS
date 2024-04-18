e306365d57d11a939a5e62ee52096fc59b6b55a0
Lucas Guzman <lucas_guzman@hotmail.es>
2024-04-18 20:52:35 +0200

    format

--- queue.c
+++ queue.c
@@ -1,51 +1,45 @@
-//SSOO-P3 23/24
+// SSOO-P3 23/24
 
+#include "queue.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
-#include "queue.h"
 
-//To create a queue
-queue* queue_init(int size)
-{
+// To create a queue
+queue *queue_init(int size) {
 
-  queue * q = (queue *)malloc(sizeof(queue));
+    queue *q = (queue *)malloc(sizeof(queue));
 
-  return q;
+    return q;
 }
 
 // To Enqueue an element
-int queue_put(queue *q, struct element* x)
-{
-  
-  return 0;
+int queue_put(queue *q, struct element *x) {
+
+    return 0;
 }
 
 // To Dequeue an element.
-struct element* queue_get(queue *q)
-{
-  struct element* element;
-  
-  return element;
+struct element *queue_get(queue *q) {
+    struct element *element;
+
+    return element;
 }
 
-//To check queue state
-int queue_empty(queue *q)
-{
-  
-  return 0;
+// To check queue state
+int queue_empty(queue *q) {
+
+    return 0;
 }
 
-int queue_full(queue *q)
-{
-  
-  return 0;
+int queue_full(queue *q) {
+
+    return 0;
 }
 
-//To destroy the queue and free the resources
-int queue_destroy(queue *q)
-{
+// To destroy the queue and free the resources
+int queue_destroy(queue *q) {
 
-  return 0;
+    return 0;
 }
--- store_manager.c
+++ store_manager.c
@@ -1,36 +1,29 @@
-//SSOO-P3 23/24
+// SSOO-P3 23/24
 
+#include "queue.h"
+#include <fcntl.h>
+#include <pthread.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stddef.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include "queue.h"
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
+int main(int argc, const char *argv[]) {
+    int profits = 0;
+    int product_stock[5] = {0};
 
-int main (int argc, const char * argv[])
-{
-  int profits = 0;
-  int product_stock [5] = {0};
+    // Output
+    printf("Total: %d euros\n", profits);
+    printf("Stock:\n");
+    printf("  Product 1: %d\n", product_stock[0]);
+    printf("  Product 2: %d\n", product_stock[1]);
+    printf("  Product 3: %d\n", product_stock[2]);
+    printf("  Product 4: %d\n", product_stock[3]);
+    printf("  Product 5: %d\n", product_stock[4]);
 
-
-
-
-
-
-  // Output
-  printf("Total: %d euros\n", profits);
-  printf("Stock:\n");
-  printf("  Product 1: %d\n", product_stock[0]);
-  printf("  Product 2: %d\n", product_stock[1]);
-  printf("  Product 3: %d\n", product_stock[2]);
-  printf("  Product 4: %d\n", product_stock[3]);
-  printf("  Product 5: %d\n", product_stock[4]);
-
-  return 0;
+    return 0;
 }
